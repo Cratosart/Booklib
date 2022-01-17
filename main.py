206d1720cbbc66fb4a37e3235d8b5c89c4b7f33b
Artyom <artem.a.dyachenko@gmail.com>
2022-01-17 20:15:17 +0300

    download book covers.

--- main.py
+++ main.py
@@ -16,32 +16,29 @@ def check_for_redirect(book):
 
 
 def loading_book_content(url, id):
-        payload = {
-            'id': {id}
-        }
-        book = requests.get(url, params=payload)
-        book.raise_for_status()
-        if book.history == []:
-            try:
-                url = f'https://tululu.org/b{id}/'
-                response = requests.get(url)
-                response.raise_for_status()
-                soup = BeautifulSoup(response.text, 'lxml')
-                title_tag = (soup.find('body').find('h1'))
-                title_text = title_tag.text
-                info_book = title_text.split('::')
-                name_book = info_book[0].strip()
-                name_book = f'{sanitize_filename(name_book)}'
-                book_author = info_book[1].strip()
-                download_txt(book.content, name_book, id)
-                img_book = (soup.find('div', class_='bookimage').find('img')['src'])
-                url_img = urllib.parse.urlsplit(img_book)
-                split_url = splitext(url_img.path)
-                extension = split_url[1]
-                path_url_img = urljoin(url, img_book)
-                download_image(path_url_img, id, extension, book.content)
-            except:
-                check_for_redirect(book)
+    payload = {
+        'id': {id}
+    }
+    book = requests.get(url, params=payload)
+    book.raise_for_status()
+    if book.history == []:
+        url = f'https://tululu.org/b{id}/'
+        response = requests.get(url)
+        response.raise_for_status()
+        soup = BeautifulSoup(response.text, 'lxml')
+        title_tag = (soup.find('body').find('h1'))
+        title_text = title_tag.text
+        info_book = title_text.split('::')
+        name_book = info_book[0].strip()
+        name_book = f'{sanitize_filename(name_book)}'
+        book_author = info_book[1].strip()
+        download_txt(book.content, name_book, id)
+        img_book = (soup.find('div', class_='bookimage').find('img')['src'])
+        url_img = urllib.parse.urlsplit(img_book)
+        split_url = splitext(url_img.path)
+        extension = split_url[1]
+        path_url_img = urljoin(url, img_book)
+        download_image(path_url_img, id, extension, book.content)
 
 
 def download_txt(content, name_book, id):
@@ -57,9 +54,11 @@ def download_image(path_url_img, id, extension, content):
     path_img_books = 'images'
     os.makedirs(path_img_books, exist_ok=True)
     filename = f'{id}{extension}'
+    img = requests.get(path_url_img)
     save_path = os.path.join(path_img_books, filename)
-    with open(filename, 'wb') as file:
-        file.write(content.read)
+    with open(save_path, 'wb') as file:
+        file.write(img.content)
+
 
 
 
