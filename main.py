26e4cbd95e9cc5ac69b359f18b31edb8ce5f9225
Artyom <artem.a.dyachenko@gmail.com>
2022-02-07 21:09:20 +0300

    Changed the structure of the code

--- main.py
+++ main.py
@@ -10,51 +10,36 @@ from pathvalidate import sanitize_filename
 from urllib.parse import urljoin
 
 
-def createparser ():
+def createParser ():
     parser = argparse.ArgumentParser()
-    parser.add_argument('start_id', nargs='?', default=1, type=int)
-    parser.add_argument('end_id', nargs='?', default=10, type=int)
+    parser.add_argument('start_id', nargs='?', default=1)
+    parser.add_argument('end_id', nargs='?', default=10)
     return parser
 
 
-def loading_book_content(url, id):
-    payload = {                                     # начало запроса
-        'id': {id}
-    }
-    book = requests.get(url, params=payload)
-    book.raise_for_status()                         # Запрос к библиотеке по адресу и id книги
+def download_txt(url_book, id, url_content):
+    payload = {
+            'id': {id}
+        }
+    book = requests.get(url_book, params=payload)
+    book.raise_for_status()
+    print(url_content)
     if book.history == []:
-        url = f'https://tululu.org/b{id}/'
-        response = requests.get(url)
-        response.raise_for_status()
-        soup = BeautifulSoup(response.text, 'lxml')         # тут мы преобразуем страницу контента в суп
-        title_tag = soup.find('body').find('h1')          # парсим название
-        title_text = title_tag.text                         # записываем в переменную
-        info_book = title_text.split('::')                   # определяем разделитель
-        name_book = info_book[0].strip()                    # удаляем пробелы
-        name_book = f'{sanitize_filename(name_book)}'       # нормализуем названием книги
-        book_author = info_book[1].strip()                  # Забираем автора из разделенного тайтла
-        download_txt(book.content, name_book, id)
-        img_book = soup.find('div', class_='bookimage').find('img')['src']
-        url_img = urllib.parse.urlsplit(img_book)           # работаем со ссылкой на изображение
-        split_url = splitext(url_img.path)                   # отделяем нужную часть ссылки
-        extension = split_url[1]                            # забираем 2 элемент списка
-        path_url_img = urljoin(url, img_book)               # соединяем с основным адресом наш элемент
-        download_image(path_url_img, id, extension, book.content)
-        parse_book_page(soup)
+        book_author, book_name = parse_book_page(url_content)
+        save_book(book.content, book_name, id)
+        get_comment(url_content)
+        download_image(url_content)
 
 
-
-def download_txt(content, name_book, id):
-    path_books = 'Books'
-    os.makedirs(path_books, exist_ok=True)
-    filename = f'{id}. {name_book}.txt'
-    save_path = os.path.join(path_books, filename)
-    with open(f'{save_path}', 'wb') as file:
-        file.write(content)
-
-
-def download_image(path_url_img, id, extension, content):
+def download_image(url_content):
+    response = requests.get(url_content)
+    response.raise_for_status()
+    soup = BeautifulSoup(response.text, 'lxml')
+    img_book = (soup.find('div', class_='bookimage').find('img')['src'])
+    url_img = urllib.parse.urlsplit(img_book)
+    split_url = splitext(url_img.path)
+    extension = split_url[1]
+    path_url_img = urljoin(url_content, img_book)
     path_img_books = 'images'
     os.makedirs(path_img_books, exist_ok=True)
     filename = f'{id}{extension}'
@@ -64,30 +49,47 @@ def download_image(path_url_img, id, extension, content):
         file.write(img.content)
 
 
-def parse_book_page(soup):
-    title_tag = soup.find('body').find('h1')
-    title_text = title_tag.text
-    info_book = title_text.split('::')
-    name_book = info_book[0].strip()
-    name_book = f'{sanitize_filename(name_book)}'
-    print(name_book)
-    book_author = info_book[1].strip()
-    print(book_author)
+def get_comment(url_content):
+    response = requests.get(url_content)
+    response.raise_for_status()
+    soup = BeautifulSoup(response.text, 'lxml')
     comment_text = (soup.find_all('span', class_='black'))
     if comment_text:
         for comment in comment_text:
             print(comment.text)
-    book_genre = soup.find('span', class_='d_book')
+    book_genre = (soup.find('span', class_='d_book'))
     print(book_genre.text)
 
+def save_book(content, book_name, id):
+    path_books = 'Books'
+    os.makedirs(path_books, exist_ok=True)
+    filename = f'{id}. {book_name}.txt'
+    save_path = os.path.join(path_books, filename)
+    with open(f'{save_path}', 'wb') as file:
+        file.write(content)
+
+
+def parse_book_page(url_content):
+    response = requests.get(url_content)
+    response.raise_for_status()
+    soup = BeautifulSoup(response.text, 'lxml')
+    title_tag = (soup.find('body').find('h1'))
+    title_text = title_tag.text
+    info_book = title_text.split('::')
+    name_book = info_book[0].strip()
+    name_book = f'{sanitize_filename(name_book)}'
+    book_author = info_book[1].strip()
+    return book_author, name_book
 
 
 
 if __name__ == '__main__':
-    parser = createparser()
-    args = parser.parse_args(sys.argv[1:])
+    parser = createParser()
+    namespace = parser.parse_args(sys.argv[1:])
+    print(namespace)
     url_book = 'http://tululu.org/txt.php'
-    for id_books in range(args.start_id, args.end_id+1):
-        loading_book_content(url_book, id_books)
-
-
+    url_content = 'https://tululu.org/b'
+    for id in range(int(namespace.start_id), int(namespace.end_id)+1):
+        # loading_book_content(url_book, id)
+        url_content = f'https://tululu.org/b{id}/'
+        download_txt(url_book, id, url_content)
